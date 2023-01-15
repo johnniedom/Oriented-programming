61131adeaa9a103c906b7a76c54f6fff224fec0a
JohnnieDom <johnniedom1@gmail.com>
2023-01-15 21:52:15 +0100

    add-up

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// TOPIC Constructor
+const Person = function(firstName, birthYear){
+
+}
+new Person(`Johnnie`, 2003) 
+
+// when using constructor
+/*
+1. New {object} is created
+2. function is  c
+*/
