d1f2da4e1dc489012991cd759c7cb07cce9390a0
JohnnieDom <johnniedom1@gmail.com>
2023-01-16 20:12:33 +0100

    oop

---
+++ prototypeChain.md
@@ -0,0 +1,15 @@
+                      Prototype chain
+
+The prototype chain is a mechanism used in JavaScript to look up properties and methods of an object. When you try to access a property or method of an object, the JavaScript engine first looks for it on the object itself. If it can't find it, it looks for it on the object's prototype, and if it still can't find it, it looks for it on the prototype's prototype, and so on, following the prototype chain.
+
+This allows you to create a hierarchy of objects, where objects lower in the hierarchy inherit properties and methods from objects higher up in the hierarchy. This can be used to create reusable code and can help to reduce code duplication.
+
+For example, you can have an object called "Animals" that has properties and methods that are common to all animals, such as "breathe" and "eat". Then, you can create other objects such as "Dog" and "Cat" that inherit from the "Animals" object and add their own properties and methods.
+
+To understand this better, you can think of the prototype chain like a big family tree. Each object is like a person and it inherits properties and methods from its parents, grandparents, and so on, that are represented by the prototypes, just like a person inherits traits from its ancestors.
+
+For example, when you look for a trait like "eyecolor" in your family, you first look for it in yourself, if you don't have it, you look for it in your parents, if they don't have it, you look for it in your grandparents and so on, following the family tree hierarchy.
+
+It's worth noting that JavaScript has a built-in object called Object.prototype that is at the top of the prototype chain, which means that all objects in JavaScript inherit properties and methods from this object, unless they are overwritten or shadowed.
+
+Also, it's important to note that the prototype chain is used to look up properties and methods, not to set them. When you add a property or method to an object's prototype, it's available on all objects that inherit from that prototype.
--- script.js
+++ script.js
@@ -1,13 +1,69 @@
 'use strict';
 
 // TOPIC Constructor
-const Person = function(firstName, birthYear){
-
-}
-new Person(`Johnnie`, 2003) 
-
 // when using constructor
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.yearBirth = birthYear;
+
+  // you also create a method function inside constructor function
+  // it is not good practice
+
+  // this
+};
+
 /*
-1. New {object} is created
-2. function is  c
+what happens when you call a function using the New Keyword
+1. A New {object} is crated . this = {}
+2. function is called, and then the this keyword equals newly created {object}
+3. {} linked to protoType (This is where __proto__ property is created)
+4.  The function wil automatically return {}
 */
+
+// u can then you that same function to create many object as yo want
+const Johnnie = new Person(`Johnnie`, 2003);
+const Nonye = new Person(`Nonye`, 2004);
+const Chimdindu = new Person(`Chimdindu`, 2008);
+
+console.log(Johnnie, Nonye, Chimdindu);
+// console.log(Johnnie.calAge);
+
+// TOPIC Prototypes
+// very function in javascript have Prototype property
+// the Objects that are created through constructor
+// function do have access to the prototype method and property of that
+// constructor function.
+Person.prototype.calAge = function () {
+  console.log(2023 - this.yearBirth);
+};
+
+Johnnie.calAge();
+Nonye.calAge();
+Chimdindu.calAge();
+
+// this possible because very object have access
+// To the method and properties from its prototype
+//which here Person.prototype 👇🏽👇🏽
+console.log(Johnnie.__proto__);
+
+// Confirmation the person.prototype is prototype of Johnnie
+// it is the prototype of linked Object
+console.log(Johnnie.__proto__ === Person.prototype); // true
+console.log(Person.prototype.isPrototypeOf(Johnnie)); // prototypeOfLinkedObject
+
+// you also set properties
+Person.prototype.Surname = 'Modebe';
+
+console.log(Johnnie.Surname, Chimdindu.Surname);
+
+// checking properties
+console.log(Johnnie.hasOwnProperty(`firstName`));
+console.log(Johnnie.hasOwnProperty(`Surname`));
+
+console.log(Johnnie.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+//working with Arrays
+const arr = [2,5,7,65,4,4]
+console.log(arr.__proto__);
